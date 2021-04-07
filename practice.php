11dfaa3b1d6c2502751398f3b38f897ffbfff529
’名前” <>
2021-04-07 07:21:47 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,25 @@
+<?php
+echo 'hello php!';
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "Riki";
+$world = "'s World!";
+echo $hello.$name.$world;
+
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo $tech_boost;
+
+$calendar = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+// 12月を表示する
+echo $calendar[11];
+
+
+
